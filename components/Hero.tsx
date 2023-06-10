91561b7975a0a171a4f5e582c71664b1ea415f61
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-06-10 07:32:01 +0200

    images and folder components added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,10 @@ const Hero = () => {
         />
       </div>
       <div className="hero__image-container">
-        <div className="hero__image">{/* <Image src="" /> */}</div>
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   );
